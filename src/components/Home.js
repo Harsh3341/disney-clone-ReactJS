1f8a68634237fc00727e4fdba7af278f5435f4d3
Harsh Gupta <harshgupta3341@gmail.com>
2022-05-31 21:14:39 +0530

    Added Recommends Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import ImgSlider from './imgSlider';
+import Recommonds from './Recommends';
 import Viewers from './Viewers';
 
 function Home() {
@@ -7,6 +8,7 @@ function Home() {
         <Container>
             <ImgSlider />
             <Viewers />
+            <Recommonds />
         </Container>
     )
 }
